255defe3e92013514a40370f597de118e5a830e8
Aquila-Ace <133139373+Aquila-Ace@users.noreply.github.com>
2023-07-06 15:03:30 -0500

    Create pytech_queries.py

---
+++ module_5/pytech_queries.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.2pmbegp.mongodb.net/pytech"
+client = MongoClient(url)
+db = client.pytech
+docs = db.students.distinct('student_id')
+docs2 = db.students.distinct('first_name')
+docs3 = db.students.distinct('last_name')
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for x,y,z in zip(docs,docs2,docs3):
+    print(f"Student ID: {x}")
+    print(f"First Name: {y}")
+    print(f"Last Name: {z}")
+    print()
+
+doc = db.students.find_one({"student_id": "1007"})
+print("\n\n-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
+print(f"Student ID: {doc['student_id']}")
+print(f"First Name: {doc['first_name']}")
+print(f"Last Name: {doc['last_name']}")
