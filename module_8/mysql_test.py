fad9b005ecf54c3df173f2683cf8f5bed70554b6
Aquila-Ace <133139373+Aquila-Ace@users.noreply.github.com>
2023-07-23 16:08:43 -0500

    Update mysql_test.py

--- module_8/mysql_test.py
+++ module_8/mysql_test.py
@@ -1,30 +1,28 @@
 #As provided by Professor Haas at URL: https://cyberactive.bellevue.edu/ultra/courses/_518940_1/cl/outline
 import mysql.connector
 from mysql.connector import errorcode
-
+#Configuring based on my system and my MySQL root access
 config = {
-    "user": "pysports_user",
-    "password": "Aquila001!",
+    "user": "root",
+    "password": "DarkHorse001!",
     "host": "localhost",
     "database": "pysports",
     "raise_on_warnings": True
 }
 
-try:
+#Attempting to connect with MySQL.
+try: 
     db = mysql.connector.connect(**config)
-
-    print("\n Database user {} connected to MysSQL host {} with database {}".format(config["host"], config["database"]))
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
     input("\n\n Press any key to continue...")
-
+    
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print(" The supplied username are invalid")
-
-    elif err.errno == errorcode.ER_BAD_DB_Error:
-        print(' The specified database does not exist')
-
+        print(" The supplies username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")    
     else:
         print(err)
-    
-    #finally:
-        db.close()
+        
+finally:
+    db.close()
