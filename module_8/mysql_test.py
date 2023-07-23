1c1ae73c71d1c4846a5b3ff42b688bf5e0b7503c
Aquila-Ace <133139373+Aquila-Ace@users.noreply.github.com>
2023-07-23 15:39:21 -0500

    Update mysql_test.py

--- module_8/mysql_test.py
+++ module_8/mysql_test.py
@@ -1,1 +1,30 @@
+#As provided by Professor Haas at URL: https://cyberactive.bellevue.edu/ultra/courses/_518940_1/cl/outline
+import mysql.connector
+from mysql.connector import errorcode
 
+config = {
+    "user": "pysports_user",
+    "password": "Aquila001!",
+    "host": "localhost",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\n Database user {} connected to MysSQL host {} with database {}".format(config["host"], config["database"]))
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_Error:
+        print(' The specified database does not exist')
+
+    else:
+        print(err)
+    
+    #finally:
+        db.close()
